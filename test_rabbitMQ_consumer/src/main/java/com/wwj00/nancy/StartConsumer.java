e31951cd8da0a2b65297276da33423de657c1fe7
Nancy <wangwei_jun@163.com>
2019-07-29 22:46:51 +0800

    pom

---
+++ test_rabbitMQ_consumer/src/main/java/com/wwj00/nancy/StartConsumer.java
@@ -0,0 +1,14 @@
+package com.wwj00.nancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Nancy on 2019/7/29 18:26
+ */
+@SpringBootApplication
+public class StartConsumer {
+    public static void main(String[] args) {
+        SpringApplication.run(StartConsumer.class,args);
+    }
+}
