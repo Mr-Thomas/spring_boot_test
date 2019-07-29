be58dce8bf449adc8afffa02e2e1b5884aa5eeb3
Nancy <wangwei_jun@163.com>
2019-07-29 16:53:26 +0800

    @ConfigurationProperties(prefix="person") 注解的使用

--- spring_boot_test/src/main/java/com/wwj00/controller/QuickController.java
+++ spring_boot_test/src/main/java/com/wwj00/controller/QuickController.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Nancy on 2019/7/29 14:14
+ *
+ * @ConfigurationProperties使用:
+ *      1).需要get set方法
+ *      2).读取配置文件 prefix = "person" 表示SpringBoot配置文件中的前缀，SpringBoot会将配置文件中以server开始的属性映射到该类的字段中
  */
 @Data
 @Slf4j
 @Controller
 @ResponseBody
-@ConfigurationProperties(prefix="person") //读取配置文件 prefix = "person" 表示SpringBoot配置文件中的前缀，SpringBoot会将配置文件中以server开始的属性映射到该类的字段中
+@ConfigurationProperties(prefix="person")
 public class QuickController {
 
     private String name;
